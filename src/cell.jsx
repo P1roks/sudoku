77f7a283b127455297bce2cd90b26f9fa3d09561
P1roks <piotrekjakobczyk1@gmail.com>
2022-12-05 16:06:54 +0100

    random cells generated; not checkd if correct now

--- src/cell.jsx
+++ src/cell.jsx
@@ -2,10 +2,10 @@ import React from "react"
 import "./cells.css"
 
 export const Cell = props => {
-    let tiles = props.tiles.map(tileNo => (
+    let tiles = props.tiles.map((tileNo,idx) => (
         tileNo === 0 ?
-        <input type="number" name="" id="" /> :
-        <input type="number" name="" id="" value={tileNo} disabled /> 
+        <input type="number" name={`${props.no}${idx}`} id={`${props.no}${idx}`} /> :
+        <input type="number" name={`${props.no}${idx}`} id={`${props.no}${idx}`} value={tileNo} disabled /> 
     ))
 
     return (
