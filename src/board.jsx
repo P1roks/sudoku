9e9dc5bd048a448af9a2bbf7967e6ffeba325182
P1roks <piotrekjakobczyk1@gmail.com>
2022-12-12 13:35:29 +0100

    working coordinates in the form of (x)(y)

--- src/board.jsx
+++ src/board.jsx
@@ -5,28 +5,34 @@ import "./board.css"
 import { genCell } from "./utils";
 
 export function Board(){
-    let [tiles,getTiles] = useState([
-        genCell(),
-        genCell(),
-        genCell(),
-        genCell(),
-        genCell(),
-        genCell(),
-        genCell(),
-        genCell(),
-        genCell(),
-    ])
-        // [[3,1,6,5,2,9,4,8,7],
-        // [5,7,8,1,3,4,6,2,9],
-        // [4,9,2,7,6,8,5,3,1],
-        // [2,6,3,9,7,4,8,5,1],
-        // [4,1,5,8,6,3,7,9,2],
-        // [9,8,7,0,2,5,6,4,3],
-        // [1,3,8,6,9,2,7,4,5],
-        // [9,4,7,3,5,1,2,8,6],
-        // [2,5,6,8,7,4,3,1,9]])
+    // let [tiles,setTiles] = useState([
+    //     genCell(),
+    //     genCell(),
+    //     genCell(),
+    //     genCell(),
+    //     genCell(),
+    //     genCell(),
+    //     genCell(),
+    //     genCell(),
+    //     genCell(),
+    // ])
+    let [tiles,setTiles] = useState(
+        [[3,1,6,5,2,9,4,8,7],
+        [5,7,8,1,3,4,6,2,9],
+        [4,9,2,7,6,8,5,3,1],
+        [2,6,3,9,7,4,8,5,1],
+        [4,1,5,8,6,3,7,9,2],
+        [9,8,7,1,2,5,6,4,3],
+        [1,3,8,6,9,2,7,4,5],
+        [9,4,7,3,5,1,2,8,6],
+        [2,5,6,8,7,4,3,1,9]])
 
-    let cells = tiles.map((tile,no) => <Cell key = {no} no = {no} tiles = {tile} />)
+        //since 0 % 3 = 0 , we need to offset that
+    let temp = -3;
+    let cells = tiles.map((tile,no) => {
+    temp += (no) % 3 === 0 ? 3 : 0; 
+    return (
+    <Cell key = {no}  y = {temp} tiles = {tile} rStart = {no % 3 * 3} />)})
 
     return (
         <div id="sudoku">
--- src/cell.jsx
+++ src/cell.jsx
@@ -2,11 +2,21 @@ import React from "react"
 import "./cells.css"
 
 export const Cell = props => {
-    let tiles = props.tiles.map((tileNo,idx) => (
-        tileNo === 0 ?
-        <input type="number" name={`${props.no}${idx}`} id={`${props.no}${idx}`} /> :
-        <input type="number" name={`${props.no}${idx}`} id={`${props.no}${idx}`} value={tileNo} disabled /> 
-    ))
+    let y = props.y - 1;
+    // let tiles = props.tiles.map((tileNo,x) => (
+    //     tileNo === 0 ?
+    //     <input type="number" name={`${props.y}${x % 3 + props.rStart}`} id={`${props.y}${x % 3 + props.rStart}`} /> :
+    //     <input type="number" name={`${props.y}${x % 3 + props.rStart}`} id={`${props.y}${x % 3 + props.rStart}`} value={tileNo} disabled /> 
+    // ))
+
+    let tiles = props.tiles.map((tileNo,x) => {
+        y += x % 3 == 0 ? 1 : 0;
+        return (
+            tileNo === 0 ?
+            <input type="number" name={`${y}${x % 3 + props.rStart}`} id={`${y}${x % 3 + props.rStart}`} /> :
+            <input type="number" name={`${y}${x % 3 + props.rStart}`} id={`${y}${x % 3 + props.rStart}`} value={tileNo} disabled /> 
+        )
+    })
 
     return (
         <div className="cell">
