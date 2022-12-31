aa127b0df6824446f0d8d436594aa7aa769f3ad3
P1roks <piotrekjakobczyk1@gmail.com>
2022-12-31 19:43:41 +0100

    Timer

--- src/board.tsx
+++ src/board.tsx
@@ -5,18 +5,22 @@ import "./board.css"
 import { useRef } from "react";
 import {randomizeSudoku} from "./randomizer"
 import JSConfetti from "js-confetti";
+import {Timer} from "./timer";
 
 export function Board(props: {toDissapear: number}){
+    let [tiles,setTiles] = useState<number[][]>([[]])
+
     const formRef = useRef<HTMLFormElement>(null);
     const submitRef = useRef<HTMLButtonElement>(null);
     const resultRef = useRef<HTMLDivElement>(null);
+    const timeRef = useRef<Timer>(null);
 
     function check(event: React.FormEvent){
-    	if(!submitRef.current || !resultRef.current || !formRef.current) return;
+    	if(!submitRef.current || !resultRef.current || !formRef.current || !timeRef.current) return;
+        timeRef.current.stopTimer()
         event.preventDefault();
         const formInputs = [...formRef.current.elements].filter(elem => elem instanceof HTMLInputElement && !elem.disabled && elem.type === "number") as HTMLInputElement[]
         submitRef.current.disabled = true;
-
         let correct: number = 0;
         formInputs.forEach(inp => {
             if(inp.valueAsNumber === invis.get(inp.id)){
@@ -34,15 +38,17 @@ export function Board(props: {toDissapear: number}){
         }
 	else{resultRef.current.innerHTML = `Uzyskałeś ${correct}/${invis.size} punktów! <br /> Stanowi to ${(correct / invis.size * 100).toFixed(1)}%`;}
     }
+
     function clear(event: React.FormEvent){
-    	if(!submitRef.current || !resultRef.current || !formRef.current) return;
+    	if(!submitRef.current || !resultRef.current || !formRef.current || !timeRef.current) return;
+	timeRef.current.resetTimer();
         event.preventDefault();
         const formInputs = [...formRef.current.elements].filter(elem => elem instanceof HTMLInputElement && !elem.disabled && elem.type === "number") as HTMLInputElement[] //get all inputted numbers
         submitRef.current.disabled = false; //enable submit button
     	formInputs.forEach(inp => {inp.valueAsNumber = NaN; inp.className = "";}) //reset all inputs
 	resultRef.current.innerHTML = ``;
     }
-    let [tiles,setTiles] = useState<number[][]>([[]])
+
     useEffect(() => {
             setTiles(randomizeSudoku() || [[]])
     },[])
@@ -52,6 +58,8 @@ export function Board(props: {toDissapear: number}){
     return <Row key={no}  y={no} tiles={tile} toDissapear={props.toDissapear}/>;})
 
     return (
+    <div id="main">
+    	<Timer ref={timeRef} />
         <form ref={formRef} onSubmit={check} onReset={clear} id="sudoku">
                 <div id="board">
                     {cells}
@@ -62,5 +70,6 @@ export function Board(props: {toDissapear: number}){
                     <button type="reset">Reset</button>
                 </div>
         </form>
+    </div>
     )
 }
---
+++ src/timer.tsx
@@ -0,0 +1,33 @@
+import {Component} from "react"
+
+//Class and not a function because I need a ref to it and this seems better than forwarding
+export class Timer extends Component<{},{time: number}>{
+	timeInterval: any;
+	constructor(props: {}){ //props are unused but TS needs them for some reason
+		super(props);
+		this.state = {time: 0};
+	}
+
+	componentDidMount(): void {
+		this.timeInterval = setInterval(() => this.setState((prev:{time: number}) => ({time: prev.time + 1})),1000);
+	}
+
+	componentWillUnmount(): void { clearInterval(this.timeInterval) }
+
+	stopTimer(){ clearInterval(this.timeInterval) }
+	
+	//setting interval again is needed bcs someone can submit and then reset
+	resetTimer(){ 
+		clearInterval(this.timeInterval) 
+		this.setState(() => ({time: 0})) 
+		this.timeInterval = setInterval(() => this.setState((prev:{time: number}) => ({time: prev.time + 1})),1000);
+	}
+
+	render(){
+		let time = this.state.time;
+		let fancyTime: string;
+		if(time >= 60) fancyTime = `${Math.floor(time/60)}:${(time % 60).toString().padStart(2,'0')}`
+		else fancyTime = time.toString()
+		return <div id="timer"><p>Twój czas: {fancyTime}</p></div> 
+	}
+}
